9b7cb81a152baa4000e12de2c88e36b65c615d1f
kumarsenthil <kumar.senthilgs@gmail.com>
2019-12-01 17:37:41 -0500

    Added path.module

--- main.tf
+++ main.tf
@@ -6,14 +6,14 @@ provider "aws" {
 # Archive the code or project that we want to run
 data "archive_file" "lambda_zip" {
     type          = "zip"
-    source_file   = "index.js"
-    output_path   = "lambda_function.zip"
+    source_file   = "${path.module}/index.js"
+    output_path   = "${path.module}/lambda_function.zip"
 }
 
 # Create the function
 resource "aws_lambda_function" "cloudtrail_lambda" {
   #count = "${var.autoenable_cloudtrail ? 1 : 0}"
-  filename         = "lambda_function.zip"
+  filename         = "${path.module}/lambda_function.zip"
   function_name    = "cloudtrail_lambda"
   role             = "${aws_iam_role.iam_for_lambda_tf.arn}"
   handler          = "index.handler"
