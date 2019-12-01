aefc079d1c8aafac6ca19ae53c269165aa357b41
kumarsenthil <kumar.senthilgs@gmail.com>
2019-12-01 18:06:24 -0500

    Adding cloudwatch rule & target

--- main.tf
+++ main.tf
@@ -43,6 +43,7 @@ resource "aws_iam_role" "iam_for_lambda_tf" {
 EOF
 }
 
+#Cloudwatch log permission policy
 resource "aws_iam_policy" "lambda_logging" {
   name = "lambda_logging"
   path = "/"
@@ -66,7 +67,45 @@ resource "aws_iam_policy" "lambda_logging" {
 EOF
 }
 
+#Policy attachment to the role
 resource "aws_iam_role_policy_attachment" "lambda_logs" {
   role = "${aws_iam_role.iam_for_lambda_tf.name}"
   policy_arn = "${aws_iam_policy.lambda_logging.arn}"
+}
+
+resource "aws_cloudwatch_event_rule" "enable_cloudtrail_rule" {
+    name = "enable_cloudtrail_rule"
+    description = "Enable cloudtrail if it is disabled"
+    event_pattern = <<PATTERN
+{
+  "source": [
+    "aws.cloudtrail"
+  ],
+  "detail-type": [
+    "AWS API Call via CloudTrail"
+  ],
+  "detail": {
+    "eventSource": [
+      "cloudtrail.amazonaws.com"
+    ],
+    "eventName": [
+      "stopLogging"
+    ]
+  }
+}
+PATTERN
+}
+
+resource "aws_cloudwatch_event_target" "enable_cloudtrail_target" {
+    rule = "${aws_cloudwatch_event_rule.enable_cloudtrail_rule.name}"
+    target_id = "${aws_lambda_function.cloudtrail_lambda.function_name}"
+    arn = "${aws_lambda_function.cloudtrail_lambda.arn}"
+}
+
+resource "aws_lambda_permission" "allow_cloudwatch_to_call_lambda" {
+    statement_id = "AllowExecutionFromCloudWatch"
+    action = "lambda:InvokeFunction"
+    function_name = "${aws_lambda_function.cloudtrail_lambda.function_name}"
+    principal = "events.amazonaws.com"
+    source_arn = "${aws_cloudwatch_event_rule.enable_cloudtrail_rule.arn}"
 }
